4f3bd6a8b80ed5a78fd12fa25b79bd9ac936a66e
Jonathan Cherry <tuo97200@temple.edu>
2024-01-19 14:58:19 -0500

    Updates

--- print_random.c
+++ print_random.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
-char randchar();
+#include "random.c"
 
 int main()
 {
--- random.c
+++ random.c
@@ -1,15 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 #define NUM 26
 
 char randchar(){
     char letters[NUM] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-    //seeding the randomizer
-    srand((unsigned)time(NULL));
-
     return letters[rand() % NUM];
-
 }
